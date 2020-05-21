cefffe87191815af793a69678a780b2df5b0dbac
Adriano Avelino <adriano-st777@hotmail.com>
2020-05-21 17:02:49 -0300

    adiciona teste para adicionar elemento no vetor

--- src/main/java/com/loiane/estruturadados/vetor/Vetor.java
+++ src/main/java/com/loiane/estruturadados/vetor/Vetor.java
@@ -2,9 +2,11 @@ package com.loiane.estruturadados.vetor;
 
 public class Vetor {
 	private String[] elementos;
+	private int tamanho;
 
 	public Vetor(int capacidade) {
 		this.elementos = new String[capacidade];
+		this.tamanho = 0;
 	}
 
 	public String[] getElementos() {
@@ -13,6 +15,24 @@ public class Vetor {
 
 	public void setElementos(String[] elementos) {
 		this.elementos = elementos;
+	}
+
+//	public void adiciona(String elemento) {
+//		for(int i = 0; i < this.elementos.length; i++) {
+//			if (this.elementos[i] == null) {
+//				this.elementos[i] = elemento;
+//				return;
+//			}
+//		}
+//	}
+
+	public boolean adiciona(String elemento) {
+		if (tamanho < this.elementos.length) {
+			this.elementos[tamanho] = elemento;
+			this.tamanho++;
+			return true;
+		}
+		return false;
 	}
 
 }
--- src/test/java/com/loiane/estruturadados/vetor/VetorTest.java
+++ src/test/java/com/loiane/estruturadados/vetor/VetorTest.java
@@ -12,6 +12,35 @@ public class VetorTest {
 
 		assertEquals(expected, vetor.getElementos().length);
 	}
+	
+	@Test
+	public void adicionaNovoElemento() {
+		String expected = "elemento 01";
+		Vetor vetor = new Vetor(5);
+		vetor.adiciona("elemento 01");
+		
+		assertEquals(expected, vetor.getElementos()[0]);
+	}
+	
+	@Test
+	public void adicionaDoisNovosElementos() {
+		String expectedElement1 = "elemento 01";
+		String expectedElement2 = "elemento 02";
+		Vetor vetor = new Vetor(2);
+		vetor.adiciona("elemento 01");
+		vetor.adiciona("elemento 02");
+		
+		assertEquals(expectedElement1, vetor.getElementos()[0]);
+		assertEquals(expectedElement2, vetor.getElementos()[1]);
+	}
+
+	@Test
+	public void vetorJaEstaCheio() {
+		Vetor vetor = new Vetor(1);
+
+		assertEquals(true, vetor.adiciona("elemento 01"));
+		assertEquals(false, vetor.adiciona("elemento 02"));
+	}
 
 }
 
