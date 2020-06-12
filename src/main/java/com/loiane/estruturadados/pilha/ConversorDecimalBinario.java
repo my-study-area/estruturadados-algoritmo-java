e086ca13eaae2b028e383131d62c7da0cabd4996
Adriano Avelino <adriano-st777@hotmail.com>
2020-06-12 18:28:55 -0300

    adiciona correção do exercício 07 da classe Pilha

--- src/main/java/com/loiane/estruturadados/pilha/ConversorDecimalBinario.java
+++ src/main/java/com/loiane/estruturadados/pilha/ConversorDecimalBinario.java
@@ -3,28 +3,19 @@ package com.loiane.estruturadados.pilha;
 public class ConversorDecimalBinario {
 
     public static String converter(int numero) {
-        boolean continua = true;
-        Pilha<String> valorBinarioInvertido = new Pilha<String>();
-        while (continua) {
-            if (numero % 2 == 1) {
-                valorBinarioInvertido.empilha("1");
-                numero = numero / 2;
-            } else if (numero % 2 == 0) {
-                valorBinarioInvertido.empilha("0");
-                if (numero == 0) {
-                    continua = false;
-                }
-                numero = numero / 2;
-            }
+        Pilha<Integer> binarioInvertido = new Pilha<Integer>();
+        String numeroBinario = "";
+        int resto;
+        while (numero > 0) {
+            resto = numero % 2;
+            binarioInvertido.empilha(resto);
+            numero /= 2;
         }
 
-        String valorBinario = new String();
-        while (!valorBinarioInvertido.estaVazia()) {
-            valorBinario += valorBinarioInvertido.desempilha();
+        while (!binarioInvertido.estaVazia()) {
+            numeroBinario += binarioInvertido.desempilha();
         }
-        if (valorBinario.substring(0, 1).equalsIgnoreCase("0") && valorBinario.length() > 1) {
-            return valorBinario.substring(1);
-        }
-        return valorBinario;
+
+        return (numero == 0) ? "0": numeroBinario;
     }
 }
